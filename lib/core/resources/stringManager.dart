ee0dd52df48d7a263c4a53a840fbf4d13251482d
Mark <markmarkmagdy1010@gmail.com>
2024-06-11 01:06:08 +0300

    add needed strings in String Manager file

--- lib/core/resources/stringManager.dart
+++ lib/core/resources/stringManager.dart
@@ -1,5 +1,17 @@
 class StringManager {
-static const String OnboardingWelcome = "Welcome Tasks Manager";
-static const String OnboardingStart = "Let's Start";
+  static const String OnboardingWelcome = "Welcome Tasks Manager";
+  static const String OnboardingStart = "Let's Start";
+  static const String LoginTitle = "Welcome Back";
+  static const String LoginSubtitle = "Enter Your data";
+  static const String emailHint = "Email";
+  static const String passwordHint = "Password";
 
+  static const String LoginFooterQ = "Create new account?";
+  static const String LoginBtnName = "Login";
+  static const String SignupBtnName = "Sign Up";
+
+  static const String signupTitle = "Welcome";
+  static const String signupSubtitle = "Create new account now";
+  static const String usernameHint = "Email";
+  static const String signupFooterQ = "Already have an account?";
 }
