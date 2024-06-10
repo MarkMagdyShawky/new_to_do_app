938f22b3417d09722926334d4e1ddcbe91d38977
Mark <markmarkmagdy1010@gmail.com>
2024-06-11 01:08:13 +0300

    add needed images

--- lib/core/resources/imageManager.dart
+++ lib/core/resources/imageManager.dart
@@ -1,3 +1,6 @@
 class ImageManager {
-  static const String HomeBachground = "assets/images/b11.svg";
+  static const String HomeBackground = "assets/images/b11.svg";
+  static const String LoginImage     = "assets/images/Login12.svg";
+
+  static const String signupImage = "assets/images/signup.svg";
 }
