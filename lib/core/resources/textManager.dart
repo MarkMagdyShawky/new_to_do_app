1fec536ab5da5910a57fb1d927f2a86385df3089
Mark <markmarkmagdy1010@gmail.com>
2024-06-10 10:05:52 +0300

    create text manager bright text and dark one

---
+++ lib/core/resources/textManager.dart
@@ -0,0 +1,40 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+import 'colorManager.dart';
+
+class DarkTextManager extends StatelessWidget {
+  final String text;
+  final double fontSize;
+  const DarkTextManager({super.key, required this.text, required this.fontSize});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+          color: MarkThirdColor,
+          fontSize: fontSize,
+          fontWeight: FontWeight.bold),
+    );
+  }
+}
+
+class BrightTextManager extends StatelessWidget {
+  final String text;
+  final double fontSize;
+  const BrightTextManager({super.key, required this.text, required this.fontSize});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+          color: MarkThirdColor,
+          fontSize: fontSize,
+          fontWeight: FontWeight.bold),
+    );
+  }
+}
+
