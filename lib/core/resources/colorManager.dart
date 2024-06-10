51f7e95fca50541dd79ff49ffb6233c9a08cfe28
Mark <markmarkmagdy1010@gmail.com>
2024-06-10 10:04:30 +0300

    custom color manager

---
+++ lib/core/resources/colorManager.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:flutter/material.dart';
+
+const MarkPrimaryColor = Color(0xff193251);
+
+const MarkSecondaryColor = Color(0xfffdb762);
+
+const MarkThirdColor = Color.fromARGB(255, 255, 255, 255);
+
+const MarkBackgroundColor = Colors.white;
+
+const Mark4Color = Color(0xffffc727);
