90f5d2c196d1de05f18fcbdb6c8fecd706a19ac0
Mark <markmarkmagdy1010@gmail.com>
2024-06-10 10:07:05 +0300

    setup pages

--- lib/App/myApp.dart
+++ lib/App/myApp.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_app/Features/onboarding/screens/onboardingPage.dart';
+import 'package:to_do_app/core/resources/routsManager.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -6,6 +8,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(fontFamily: "Acme-Regular"),
+      debugShowCheckedModeBanner: false,
+      routes: RoutManager.PageRoute,
+      initialRoute: "Onboarding",
     );
   }
 }
