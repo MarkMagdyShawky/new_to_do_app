35c06333b6805d7aca724170c8020f350e1bf887
Mark <markmarkmagdy1010@gmail.com>
2024-06-21 08:43:25 +0300

    add to do home page to routes

--- lib/core/resources/routsManager.dart
+++ lib/core/resources/routsManager.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:to_do_app/Features/login/screens/loginPage.dart';
 import 'package:to_do_app/Features/onboarding/screens/onboardingPage.dart';
 import 'package:to_do_app/Features/singup/screens/signupPage.dart';
+import 'package:to_do_app/Features/toDoHome/screens/toDoHomeScreen.dart';
 
 class RoutManager {
   static Map<String, WidgetBuilder> PageRoute = {
     "Onboarding": (context) => OnboardingPage(),
-    "Login" :(context) => LoginPage(),
-    "Signup" : (context) => SignupPage(),
+    "Login": (context) => LoginPage(),
+    "Signup": (context) => SignupPage(),
+    "ToDoHome": (context) => ToDoHomeScreen(),
   };
 }
