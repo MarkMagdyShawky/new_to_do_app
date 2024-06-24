f7fde397e45d8c5cc68ce44533f77f4694e6e91a
Mark <markmarkmagdy1010@gmail.com>
2024-06-24 03:13:09 +0300

    custom list title widget

--- lib/Features/tasks/widgets/customListTitleWidget.dart
+++ lib/Features/tasks/widgets/customListTitleWidget.dart
@@ -1,1 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_app/core/resources/colorManager.dart';
+import 'package:to_do_app/core/resources/textManager.dart';
 
+class CustomListTitle extends StatelessWidget {
+  final VoidCallback? handleCheckboxChange;
+  final bool isChecked;
+
+  final String taskName;
+  final String description;
+  const CustomListTitle({
+    super.key,
+    required this.handleCheckboxChange,
+    required this.isChecked,
+    required this.taskName,
+    required this.description,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: MarkBackgroundColor,
+        border: Border.all(color: Mark4Color.withOpacity(0.3)),
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            color: MarkSecondaryColor.withOpacity(0.2),
+            spreadRadius: 2,
+            blurRadius: 7,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
+      child: ListTile(
+        shape: BeveledRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        title: GoldTextManager(text: taskName, fontSize: 18),
+        subtitle: Text(description),
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Checkbox(
+              value: isChecked,
+              onChanged: (bool? value) {
+                if (handleCheckboxChange != null) {
+                  handleCheckboxChange!();
+                }
+              },
+              activeColor: MarkPrimaryColor,
+              materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.close,
+              ),
+              color: MarkPrimaryColor,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
