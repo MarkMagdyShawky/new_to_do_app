d34258247cfd4a2324dfd369c972f08dd9c658ed
Mark <markmarkmagdy1010@gmail.com>
2024-06-26 12:29:46 +0300

    add animation splash screen

--- lib/App/myApp.dart
+++ lib/App/myApp.dart
@@ -1,8 +1,12 @@
 // ignore_for_file: avoid_print
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:to_do_app/Features/onboarding/screens/onboardingPage.dart';
+import 'package:to_do_app/Features/toDoHome/screens/toDoHomeScreen.dart';
 import 'package:to_do_app/core/resources/colorManager.dart';
 import 'package:to_do_app/core/resources/routsManager.dart';
+import 'package:another_flutter_splash_screen/another_flutter_splash_screen.dart';
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
@@ -31,7 +35,17 @@ class _MyAppState extends State<MyApp> {
       debugShowCheckedModeBanner: false,
       color: MarkBackgroundColor,
       routes: RoutManager.PageRoute,
-      initialRoute: (FirebaseAuth.instance.currentUser != null) ? "ToDoHome" : "Onboarding",
+      home: AnimatedSplashScreen(
+        splashTransition: SplashTransition.scaleTransition,
+        splashIconSize: 200,
+        duration: 400,
+        splash: CircleAvatar(
+          backgroundImage: AssetImage("assets/images/logo88.png"),
+          radius: 90,
+        ),
+        nextScreen: (FirebaseAuth.instance.currentUser != null) ? ToDoHomeScreen() : OnboardingPage(),
+      ),
+      // initialRoute: (FirebaseAuth.instance.currentUser != null) ? "ToDoHome" : "Onboarding",
     );
   }
 }
